e2d8948bd3adecb406f357789108c659af6f6ce4
Hazna At Thooriqoh <hazna@wowrack.com>
2021-08-25 10:21:36 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd7db91dfbd994e3feec5763e0dd62de5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7d0839cc-06c3-4272-bf15-adf7ad0ed902'
+};
